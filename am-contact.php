f34a9ecfe154b227b0f7fd33739f28ab608627bf
Md Abdul Majid <abmajidweb@gmail.com>
2020-07-11 16:59:44 +0600

    plugin header description change.

--- am-contact.php
+++ am-contact.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name:       My Contact Form
  * Plugin URI:        https://abdulmajid.xyz
- * Description:       An easy contact form for form builder and it's a testing purpose.
+ * Description:       Easy contact form for form builder and it's a testing purpose.
  * Version:           1.0
  * Author:            Abdul Majid
  * Author URI:        https://abdulmajid.xyz
