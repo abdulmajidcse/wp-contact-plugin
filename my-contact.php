c7085922f2c3ceae75a86b0f7bf896d7ca797aa9
Md Abdul Majid <abmajidweb@gmail.com>
2020-07-11 17:19:52 +0600

    Plugin Initialize Class and main plugin function create.

--- my-contact.php
+++ my-contact.php
@@ -9,4 +9,41 @@
  * Author URI:        https://abdulmajid.xyz
  * License:           GPL v2
  * Text Domain:       my-contact
- * /
+ */
+
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+final class My_Contact {
+
+    private function __construct() {
+        # code ...
+    }
+
+    /**
+     * Initialize Plugin class
+     * 
+     * @return \My_Contact
+     */
+    public static function init() {
+        $instance = false;
+        if ( ! $instance ) {
+            $instance = new self();
+        }
+        
+        return $instance;
+    }
+}
+
+/**
+ * Create object of Main plugin class
+ * 
+ * @return \My_Contact
+ */
+function my_contact() {
+    My_Contact::init();
+}
+
+/**
+ * call plugin function
+ */
+my_contact();
