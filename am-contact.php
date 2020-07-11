6116235e9a54758524f7fffda6d539d035c7ab7b
Md Abdul Majid <abmajidweb@gmail.com>
2020-07-11 16:49:07 +0600

    My first contact form wp-plugin initialize.

---
+++ am-contact.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Plugin Name:       My Contact Form
+ * Plugin URI:        https://abdulmajid.xyz
+ * Description:       An easy contact form for form builder and it's a testing purpose.
+ * Version:           1.0
+ * Author:            Abdul Majid
+ * Author URI:        https://abdulmajid.xyz
+ * License:           GPL v2
+ * Text Domain:       my-contact
+ * /
