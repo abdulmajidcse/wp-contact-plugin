a195e01ed00bd1c0a9932b7d82003cbd14f519ff
Md Abdul Majid <abmajidweb@gmail.com>
2020-07-14 10:58:34 +0600

    include form file and redirect url.

--- includes/Frontend.php
+++ includes/Frontend.php
@@ -18,57 +18,15 @@ class Frontend {
     }
 
     public function render_shortcode( $atts, $content = '' ) {
-        ob_start();    
-    ?>
-
-        <div class="my-contact-message">
-            <form action="http://localhost/wordpress/check-message" method="POST">
-                <p>
-                    <label for="">Name</label>
-                </p>
-                <p>
-                    <input type="text" name="name" required>
-                </p>
-
-                <p>
-                    <label for="">Email</label>
-                </p>
-                <p>
-                    <input type="email" name="email" required>
-                </p>
-
-                <p>
-                    <label for="">Subject</label>
-                </p>
-                <p>
-                    <input type="text" name="subject" required>
-                </p>
-
-                <p>
-                    <label for="">Message</label>
-                </p>
-                <p>
-                    <textarea name="message" id="message" cols="30" rows="10"></textarea>
-                </p>
-
-                <?php
-                    wp_nonce_field( 'my_contact_form_nonce' );
-                ?>
-                
-                <p>
-                    <button type="submit" name="my_contact_message_send">Send</button>
-                </p>
-            </form>
-        </div>
-
-    <?php
+        ob_start();
+        require_once __DIR__ . '/views/contact-form.php';
         return ob_get_clean();
     }
 
     public function form_handler() {
-        if ( isset( $_POST['my_contact_message_send'] ) && wp_verify_nonce( $_POST['_wpnonce'], 'my_contact_form_nonce' ) ) {
-            die( var_dump( $_POST ) );
-            wp_redirect( 'http://localhost/'.$_POST['_wp_http_referer'] );
+        if ( isset( $_POST[ 'my_contact_message_send' ] ) && wp_verify_nonce( $_POST['_wpnonce'], 'my_contact_form_nonce' ) ) {
+            $redirect_to = $_POST[ 'form_url' ] . '?message=true';
+            wp_redirect( $redirect_to );
             exit;
         } else {
             wp_die( 'Are you chating?' );
---
+++ includes/views/contact-form.php
@@ -0,0 +1,49 @@
+<div class="my-contact-message">
+    <?php
+        if ( isset( $_GET[ 'message' ] ) && $_GET[ 'message' ] == 'true' ) {
+            echo 'success';
+        }
+    ?>
+    <form action="" method="POST">
+        <p>
+            <label for="">Name</label>
+        </p>
+        <p>
+            <input type="text" name="name" required>
+        </p>
+
+        <p>
+            <label for="">Email</label>
+        </p>
+        <p>
+            <input type="email" name="email" required>
+        </p>
+
+        <p>
+            <label for="">Subject</label>
+        </p>
+        <p>
+            <input type="text" name="subject" required>
+        </p>
+
+        <p>
+            <label for="">Message</label>
+        </p>
+        <p>
+            <textarea name="message" id="message" cols="30" rows="10"></textarea>
+        </p>
+
+        <?php
+            global $wp;
+            $form_url = home_url( add_query_arg( array(), $wp->request ) );
+
+            wp_nonce_field( 'my_contact_form_nonce' );
+        ?>
+
+        <input type="hidden" name="form_url" value="<?php echo $form_url; ?>">
+        
+        <p>
+            <button type="submit" name="my_contact_message_send">Send</button>
+        </p>
+    </form>
+</div>
